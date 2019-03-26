67a03cdb334a727f90b71f7d422c8da662785db0
Ivan <devskyfly@gmail.com>
2019-03-26 10:48:23 +0300

    Fix checkbox unchecked default value setting in admin-panel.

--- controllers/RatesController.php
+++ controllers/RatesController.php
@@ -60,7 +60,7 @@ class RatesController extends AbstractContentPanelController
                     .$form->field($item,'name')
                     .$form->field($item,'create_date_time')
                     .$form->field($item,'change_date_time')
-                    .$form->field($item,'active')->checkbox(['value'=>'Y','uncheckValue'=>'N','checked'=>$item->active=='Y'?true:false])
+                    .$form->field($item,'active')->checkbox(['value'=>'Y','uncheck'=>'N','checked'=>$item->active=='Y'?true:false])
                     .$form->field($item,'data')->textarea(['rows'=>10])
                 ],
                /*  [
--- controllers/RegionsController.php
+++ controllers/RegionsController.php
@@ -55,7 +55,7 @@ class RegionsController extends AbstractContentPanelController
                     $form->field($item,'name')
                     .$form->field($item,'create_date_time')
                     .$form->field($item,'change_date_time')
-                    .$form->field($item,'active')->checkbox(['value'=>'Y','uncheckValue'=>'N','checked'=>$item->active=='Y'?true:false])
+                    .$form->field($item,'active')->checkbox(['value'=>'Y','uncheck'=>'N','checked'=>$item->active=='Y'?true:false])
                     .$form->field($item,'str_nmb')
                 ]
             ];
